c5b93ff54e3c8aaa32ef668943914dd584eff12a
KingJames <jamesakpan016@gmail.com>
2021-07-30 16:50:09 +0100

    first prototype

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import {ChakraProvider, theme, ThemeProvider} from '@chakra-ui/react';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps}/>
+    </ChakraProvider>
+  )
 }
-export default MyApp
+export default MyApp;
